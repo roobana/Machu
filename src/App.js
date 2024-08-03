3d19af9a68411333fa9927ad725c838b7f17dcbf
roobana <roobanachinraj@gmail.com>
2024-08-03 09:57:45 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [show, setShow] = useState(false);
+  const [head, setHead] = useState(true);
+  const [no, setNo] = useState(false);
+
+  const handleYes = () => {
+    setShow(true);
+    setNo(false);
+    setHead(false);
+  };
+
+  const handleNo = () => {
+    setNo(true);
+    setShow(false);
+    setHead(false);
+  };
+
+  const handleGoHome = () => {
+    setShow(false);
+    setNo(false);
+    setHead(true);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {head ? (
+        <>
+          <p>Machu Sorry</p>
+          <div className="hugs"></div>
+          <div className="accept">
+            <p>Will you Accept my Sorry</p>
+            <button onClick={handleYes}>Yes</button>
+            <button onClick={handleNo}>No</button>
+          </div>
+        </>
+      ) : null}
+
+      {show && (
+        <>
+          <p>Love you Machu</p>
+          <div className="love"></div>
+          <button onClick={handleGoHome}>Go Home</button>
+        </>
+      )}
+
+      {no && (
+        <>
+          <p>Sorry da laddu kutty</p>
+          <div className="sorry"></div>
+          <button onClick={handleGoHome}>Go Home</button>
+        </>
+      )}
     </div>
   );
 }
